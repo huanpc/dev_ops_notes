324f1c240d72ea798a89d2c765d2a05406458b90
huanpc <nauh94@gmail.com>
2018-05-08 17:56:40 +0700

    nginx settup

---
+++ script/byobu_centos7.sh
@@ -0,0 +1,3 @@
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -ivh epel-release-7-5.noarch.rpm
+sudo yum install byobu -y --enablerepo=epel-testing
---
+++ script/nginx_centos7.sh
@@ -0,0 +1,3 @@
+sudo yum install epel-release
+sudo yum install -y nginx
+sudo systemctl enable nginx
